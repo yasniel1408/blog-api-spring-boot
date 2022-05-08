c85468a3081b326cd6c745b2819d6e892ed24f3b
Yasniel Fajardo Egues <yasnielfajardoegues1408@gmail.com>
2022-05-08 17:00:08 +0200

    first commit

---
+++ src/main/java/com/blog/BlogApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApiSpringBootApplication.class, args);
+    }
+
+}
